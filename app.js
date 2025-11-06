d25d00aaefc6e63aae1259b1b4066c3604a521ad
brendanharrington <brha8770@colorado.edu>
2025-11-06 12:55:33 -0700

    feat: add health check and test workflow

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
